e06c078eac3c683ee7d4a7da341e51bedc25504a
robert-wu-ms <41493644+robert-wu-ms@users.noreply.github.com>
2020-07-29 09:56:08 -0700

    speed up posture

--- assets/js/posture.js
+++ assets/js/posture.js
@@ -14,76 +14,82 @@ var flipHorizontal = false;
 
 var imageElement = document.getElementById('video');
 
-var globalnet;
+var globalNet;
+
+async function getGlobalNet()
+{
+    globalNet = await posenet.load();
+}
+
+getGlobalNet();
 
 imageElement.onloadeddata = function() {
   pictureSnap();
 }
 
-function getPose()
+async function getPose()
 {
-  posenet.load()
-  .then(function(net) {
-    const pose = net.estimateSinglePose(imageElement, {
-      flipHorizontal: true
-    });
-    return pose;
-  }).then(function(pose){
-    //console.log(pose);
-    //console.log(pose.keypoints[0].part, pose.keypoints[0].position.x, pose.keypoints[0].position.y);
+    if (globalNet)
+    {
+        globalNet.estimateSinglePose(imageElement, {
+            flipHorizontal: true
+            }).then(function(pose){
+            console.log(pose);
+            console.log(pose.keypoints[0].part, pose.keypoints[0].position.x, pose.keypoints[0].position.y);
+        
+            nose = pose.keypoints[0];
+            leftEar = pose.keypoints[3];
+            rightEar = pose.keypoints[4];
+            leftShoulder = pose.keypoints[5];
+            rightShoulder = pose.keypoints[6];
+            leftElbow = pose.keypoints[7];
+            rightElbow = pose.keypoints[8];
+        
+            if ((nose.score < .7) || (leftEar.score < .7) || (rightEar.score < .7))
+            {
+                $('#faceForward').text('Unknown');
+            }
+            else if ((rightEar.position.x - nose.position.x + 15 > nose.position.x - leftEar.position.x) &&
+                (rightEar.position.x - nose.position.x < nose.position.x - leftEar.position.x + 15))
+            {
+                $('#faceForward').text('True');
+            }
+            else
+            {
+                $('#faceForward').text('False');
+            }
+        
+            if ((leftShoulder.score < .7) || (rightShoulder.score < .7))
+            {
+                $('#parallelShoulders').text('Unknown');
+            }
+            else if ((leftShoulder.position.y + 15 > rightShoulder.position.y) &&
+                (leftShoulder.position.y < rightShoulder.position.y + 15))
+            {
+                $('#parallelShoulders').text('True');
+            }
+            else
+            {
+                $('#parallelShoulders').text('False');
+            }
 
-    nose = pose.keypoints[0];
-    leftEye = pose.keypoints[1];
-    rightEye = pose.keypoints[2];
-    leftShoulder = pose.keypoints[5];
-    rightShoulder = pose.keypoints[6];
-    leftElbow = pose.keypoints[7];
-    rightElbow = pose.keypoints[8];
-
-    if ((nose.score < .7) || (leftEye.score < .7) || (rightEye.score < .7))
-    {
-        $('#faceForward').text('Unknown');
+            if ((leftShoulder.score < .7) || (rightShoulder.score < .7) || (leftElbow.score < .7) || (rightElbow.score < .7))
+            {
+                $('#straightArms').text('Unknown');
+            }
+            else if ((leftElbow.position.x < leftShoulder.position.x) &&
+                (leftElbow.position.x + 60 > leftShoulder.position.x) &&
+                (rightElbow.position.x < rightShoulder.position.x + 60) &&
+                (rightElbow.position.x > rightShoulder.position.x))
+            {
+                $('#straightArms').text('True');
+            }
+            else
+            {
+                $('#straightArms').text('False');
+            }
+        })
     }
-    else if ((rightEye.position.x - nose.position.x + 15 > nose.position.x - leftEye.position.x) &&
-        (rightEye.position.x - nose.position.x < nose.position.x - leftEye.position.x + 15))
-    {
-        $('#faceForward').text('True');
-    }
-    else
-    {
-        $('#faceForward').text('False');
-    }
-
-    if ((leftShoulder.score < .7) || (rightShoulder.score < .7))
-    {
-        $('#parallelShoulders').text('Unknown');
-    }
-    else if ((leftShoulder.position.y + 15 > rightShoulder.position.y) &&
-        (leftShoulder.position.y < rightShoulder.position.y + 15))
-    {
-        $('#parallelShoulders').text('True');
-    }
-    else
-    {
-        $('#parallelShoulders').text('False');
-    }
-    
-    if ((leftShoulder.score < .7) || (rightShoulder.score < .7) || (leftElbow.score < .7) || (rightElbow.score < .7))
-    {
-        $('#straightArms').text('Unknown');
-    }
-    else if ((leftElbow.position.x < leftShoulder.position.x) &&
-        (leftElbow.position.x + 60 > leftShoulder.position.x) &&
-        (rightElbow.position.x < rightShoulder.position.x + 60) &&
-        (rightElbow.position.x > rightShoulder.position.x))
-    {
-        $('#straightArms').text('True');
-    }
-    else
-    {
-        $('#straightArms').text('False');
-    }
-  })
 }
 
 function pictureSnap() {
